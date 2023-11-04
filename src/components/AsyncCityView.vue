610d6e1c8b28d99fe119efee75352dd255a9434e
madani93 <madani@citynet.ir>
2023-11-05 01:07:29 +0330

    add asyncCityView component

---
+++ src/components/AsyncCityView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+
+import axios from 'axios';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const getWeatherData = async() => {
+    try {
+        const weatherData = await axios.get()
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+</script>
