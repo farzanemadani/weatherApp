c26c3f899d5cee80e605ba7c192279c3fbc1f2c0
madani93 <madani@citynet.ir>
2023-11-18 00:00:35 +0330

    complate asyncCityView component

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,6 +1,27 @@
 <template>
-    <div>
-
+    <div class="flex flex-col flex-1 items-center">
+        <!--- Banner -->
+        <div v-if="route.query.preview" class="text-white p-4 bg-weather-secondary w-full text-center">
+            <p> You are currently previewing this city, click the "+"
+            icon to start tracking this city.</p>
+        </div>
+        <!--- weather overview --->
+        <div class="flex flex-col items-center text-white py-12">
+            <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
+            <p class="text-sm mb-12">
+                {{
+                 new Date(weatherData.data.currentTime).toLocaleDateString('en-us', { weekday: 'short', day: '2-digit', month: 'long', })
+                }}
+                {{
+                new Date(weatherData.data.currentTime).toLocaleTimeString(
+                    "en-us",
+                    {
+                    timeStyle: "short",
+                    }
+                )
+                }}
+            </p>
+        </div>
     </div>
 </template>
 
@@ -12,10 +33,25 @@ import { useRoute } from 'vue-router';
 const route = useRoute();
 const getWeatherData = async() => {
     try {
-        const weatherData = await axios.get()
+        const weatherData = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial`
+    );
+    // cal current date & time
+    const localOffset = new Date().getTimezoneOffset() * 60000;
+    const utc = weatherData.data.current.dt * 1000 + localOffset;
+    weatherData.data.currentTime = utc +1000*weatherData.data.timezone_offset;
+
+    //cal hourly weather offset
+    weatherData.data.hourly.forEach((hour) => {
+        const utc = hour.dt * 1000 + localOffset;
+        hour.currentTime = utc + 1000 * weatherData.data.timezone_offset;
+    });
+    return weatherData;
     } catch (error) {
         console.log(error);
     }
-
 }
+
+const weatherData = await getWeatherData();
+console.log('weatherData', weatherData);
 </script>
