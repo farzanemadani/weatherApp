430aa80b3ca5b22c5acafefaa84a06cac0a83607
madani93 <madani@citynet.ir>
2023-12-01 23:08:24 +0330

    add transition to route

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
         <SiteNavigation/>
-        <RouterView/>
+        <RouterView v-slot="{Component}">
+            <Transition name="page" mode="out-in">
+                <component :is="Component"/>
+            </Transition>
+        </RouterView>
     </div>
   
 </template>
@@ -11,7 +15,12 @@ import { RouterView } from 'vue-router';
 import SiteNavigation from './components/SiteNavigation.vue';
 
 </script>
+<style>
+.page-enter-active {
+  transition: 600ms ease all;
+}
 
-<style lang="scss" scoped>
-
+.page-enter-from {
+  opacity: 0;
+}
 </style>
